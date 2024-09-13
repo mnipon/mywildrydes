3c6116a4ecd72f7e1e56e95f2307c15eb0b409a8
mnipon <78396926+mnipon@users.noreply.github.com>
2024-09-13 13:04:40 +0700

    Configure API InvokeURL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// Add your API invoke URL below, configured in Module 3. 
+// Add your API invoke URL below, configured in Module 3.
 
 module.exports = {
-    api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
-    }
-}
+  api: {
+    invokeUrl: "https://xl4ig3ib82.execute-api.us-east-1.amazonaws.com/dev", // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+  },
+};
